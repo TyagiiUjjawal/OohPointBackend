c9d8b248212e141c59b36ab425dddae5cbb08233
nitesh-gupta-64 <123109031@nitkkr.ac.in>
2024-07-15 13:01:21 +0530

    done

--- models/Image.js
+++ models/Image.js
@@ -2,6 +2,7 @@
 import mongoose from "mongoose";
 
 const imageSchema = new mongoose.Schema({
+  name: { type: String, required: true },
   imageUrl: { type: String, required: true },
   image: {
     data: Buffer,
