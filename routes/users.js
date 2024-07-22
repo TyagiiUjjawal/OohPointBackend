19d8851f33233491fb65c23e1185c7303e225e75
nitesh-gupta-64 <123109031@nitkkr.ac.in>
2024-07-22 13:31:14 +0530

    done

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,6 @@
 
 import express from "express";
 import {
-  addCampaign,
-  addCity,
   createUser,
   getAllUsers,
 } from "../controllers/userController.js";
@@ -12,7 +10,5 @@ const router = express.Router();
 
 router.post("/", createUser);
 router.get("/", getAllUsers);
-router.put("/", addCampaign);
-router.put("/addcity", addCity);
 
 export default router;
