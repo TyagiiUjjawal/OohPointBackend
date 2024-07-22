cfd4d19fc8619389a186d0751c3ee65ded65adba
nitesh-gupta-64 <123109031@nitkkr.ac.in>
2024-07-22 20:18:49 +0530

    done

--- routes/campaignRoutes.js
+++ routes/campaignRoutes.js
@@ -124,6 +124,7 @@ router.put("/:campaignId", async (req, res) => {
   } = req.body;
 
   try {
+    console.log(location[0].longitude + "location")
     const updatedCampaign = await Campaign.findOneAndUpdate(
       { campaignId: req.params.campaignId },
       {
